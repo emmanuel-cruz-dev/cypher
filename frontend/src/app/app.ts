7d81fa8326b1338b48c6bf7bf5408f8c9395d35a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-31 14:04:17 -0300

    feat: adds material icons registry to frontend/src/app/app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'facebook',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/facebook.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'instagram',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/instagram.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'tiktok',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/tiktok.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'x',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/x.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'youtube',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/youtube.svg')
+    );
+  }
+}
