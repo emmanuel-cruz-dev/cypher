3b53080e8887e667a6ffe9d8197c92d8e436bba2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-01 10:22:20 -0300

    feat: adds security.routes.ts to frontend/src/app/features/security/

---
+++ frontend/src/app/features/security/security.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { Security } from './pages/security';
+
+export const SECURITY_ROUTES: Routes = [
+  {
+    path: '',
+    component: Security,
+    title: 'Seguridad',
+  },
+];
