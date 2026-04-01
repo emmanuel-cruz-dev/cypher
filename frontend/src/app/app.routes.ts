d1f1e7a40ea507c4bfd6b4d46b88a6a7cb9fff43
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-01 11:09:25 -0300

    feat: adds MainLayout and new paths to frontend/src/app/app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MainLayout } from './layouts/main-layout/main-layout';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./features/home/home.routes').then((m) => m.HOME_ROUTES),
+      },
+      {
+        path: 'functions',
+        loadChildren: () =>
+          import('./features/functions/functions.routes').then((m) => m.FUNCTIONS_ROUTES),
+      },
+      {
+        path: 'security',
+        loadChildren: () =>
+          import('./features/security/security.routes').then((m) => m.SECURITY_ROUTES),
+      },
+      {
+        path: 'how-it-works',
+        loadChildren: () =>
+          import('./features/how-it-works/how-it-works.routes').then((m) => m.HOW_IT_WORKS_ROUTES),
+      },
+      {
+        path: 'about',
+        loadChildren: () => import('./features/about/about.routes').then((m) => m.ABOUT_ROUTES),
+      },
+      {
+        path: 'contact',
+        loadChildren: () =>
+          import('./features/contact/contact.routes').then((m) => m.CONTACT_ROUTES),
+      },
+    ],
+  },
+];
