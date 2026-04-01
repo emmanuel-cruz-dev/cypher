5942dbf29ef6958bf8dded7f7cbb9c847ac9ac0a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-01 10:36:37 -0300

    feat: adds users-management files to frontend/src/app/features/admin/pages/users-management

---
+++ frontend/src/app/features/admin/pages/users-management/users-management.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-management',
+  imports: [],
+  templateUrl: './users-management.html',
+})
+export class UsersManagement {}
