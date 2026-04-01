37497ab4b54112c5f51b1397708e149fe54de85f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-01 11:05:48 -0300

    feat: adds how-it-works.routes.ts to frontend/src/app/features/how-it-works/

---
+++ frontend/src/app/features/how-it-works/how-it-works.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { HowItWorks } from './pages/how-it-works';
+
+export const HOW_IT_WORKS_ROUTES: Routes = [
+  {
+    path: '',
+    component: HowItWorks,
+    title: 'Cómo funciona',
+  },
+];
