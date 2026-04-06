b93bbfda0efae61f244420b6793064521749d3fd
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-06 14:13:06 -0300

    feat: adds home components to files in frontend/src/app/features/home/pages/

--- frontend/src/app/features/home/pages/home.ts
+++ frontend/src/app/features/home/pages/home.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 
+import { HeroSection } from '../components/hero-section/hero-section';
+import { Features } from '../components/features/features';
+import { WhoWeAre } from '../components/who-we-are/who-we-are';
+import { Mission } from '../components/mission/mission';
+import { Testimonials } from '../components/testimonials/testimonials';
+import { CtaBanner } from '../../../shared/components/cta-banner/cta-banner';
+
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeroSection, Features, WhoWeAre, Mission, Testimonials, CtaBanner],
   templateUrl: './home.html',
 })
 export class Home {}
