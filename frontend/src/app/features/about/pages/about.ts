40aaf033f5c047728adb330481b7f9aed79059fe
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-07 10:29:26 -0300

    feat: adds AboutUs and OurTeam to files in frontend/src/app/features/about/pages/

--- frontend/src/app/features/about/pages/about.ts
+++ frontend/src/app/features/about/pages/about.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { AboutUs } from '../components/about-us/about-us';
+import { OurTeam } from '../components/our-team/our-team';
+
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [AboutUs, OurTeam],
   templateUrl: './about.html',
 })
 export class About {}
