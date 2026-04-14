42a395f4b61c2e91d7e79eab9e9e528a501c9f2d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-14 16:49:57 -0300

    feat: adds TranslatePipe and LanguageService to frontend/src/app/features/home/components/features/features.ts

--- frontend/src/app/features/home/components/features/features.ts
+++ frontend/src/app/features/home/components/features/features.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+
+import { LanguageService } from '../../../../core';
 
 @Component({
   selector: 'app-features',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './features.html',
 })
-export class Features {}
+export class Features {
+  readonly langService = inject(LanguageService);
+}
