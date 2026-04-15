2431b48f7cc3feb8c1519e56079478a0a2d474af
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-15 16:16:32 -0300

    feat: adds interface, attributes and imports to frontend/src/app/features/home/components/features/features.ts

--- frontend/src/app/features/home/components/features/features.ts
+++ frontend/src/app/features/home/components/features/features.ts
@@ -1,13 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgmMotionDirective } from '@scripttype/ng-motion';
 import { TranslatePipe } from '@ngx-translate/core';
 
-import { LanguageService } from '../../../../core';
+interface Feature {
+  key: string;
+  image: string;
+  delay: number;
+}
 
 @Component({
   selector: 'app-features',
-  imports: [TranslatePipe],
+  imports: [NgmMotionDirective, TranslatePipe],
   templateUrl: './features.html',
 })
 export class Features {
-  readonly langService = inject(LanguageService);
+  features: Feature[] = [
+    {
+      key: 'automation',
+      image: 'src/assets/images/home/feature-automation.png',
+      delay: 0,
+    },
+    {
+      key: 'sharing',
+      image: 'src/assets/images/home/feature-sharing.png',
+      delay: 0.1,
+    },
+    {
+      key: 'strong',
+      image: 'src/assets/images/home/feature-strong.png',
+      delay: 0.2,
+    },
+    {
+      key: 'access',
+      image: 'src/assets/images/home/feature-access.png',
+      delay: 0.3,
+    },
+  ];
 }
