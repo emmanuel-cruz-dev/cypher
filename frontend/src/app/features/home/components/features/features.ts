151e18163dbee4d58df6934028b9acc7f5ac504b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-16 09:01:24 -0300

    fix: modifies images path in frontend/src/app/features/home/components/features/features.ts

--- frontend/src/app/features/home/components/features/features.ts
+++ frontend/src/app/features/home/components/features/features.ts
@@ -13,22 +13,22 @@ export class Features {
   features = [
     {
       key: 'automation',
-      image: 'src/assets/images/home/feature-automation.png',
+      image: 'assets/images/home/feature-automation.png',
       delay: 0,
     },
     {
       key: 'sharing',
-      image: 'src/assets/images/home/feature-sharing.png',
+      image: 'assets/images/home/feature-sharing.png',
       delay: 0.1,
     },
     {
       key: 'strong',
-      image: 'src/assets/images/home/feature-strong.png',
+      image: 'assets/images/home/feature-strong.png',
       delay: 0.2,
     },
     {
       key: 'access',
-      image: 'src/assets/images/home/feature-access.png',
+      image: 'assets/images/home/feature-access.png',
       delay: 0.3,
     },
   ];
