fb222f61fe5d8ce19c8899dd0702251f66d5a39e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-24 12:20:33 -0300

    feat: adds tooltips and modifies social links in footer files in frontend/src/app/shared/components/footer/

--- frontend/src/app/shared/components/footer/footer.ts
+++ frontend/src/app/shared/components/footer/footer.ts
@@ -1,19 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslatePipe } from '@ngx-translate/core';
+
+interface FooterLink {
+  labelKey: string;
+  route: string;
+}
 
 interface FooterColumn {
   titleKey: string;
-  links: { labelKey: string; route: string }[];
+  links: FooterLink[];
 }
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, MatIconModule, TranslatePipe],
+  imports: [RouterLink, MatIconModule, MatTooltipModule, TranslatePipe],
   templateUrl: './footer.html',
 })
 export class Footer {
+  readonly socials = [
+    {
+      labelKey: 'Facebook',
+      route: 'https://www.facebook.com/',
+      icon: 'facebook',
+      color: 'blue-600',
+    },
+    {
+      labelKey: 'Instagram',
+      route: 'https://www.instagram.com/',
+      icon: 'instagram',
+      color: 'pink-600',
+    },
+    { labelKey: 'TikTok', route: 'https://www.tiktok.com/', icon: 'tiktok', color: 'white' },
+    { labelKey: 'X', route: 'https://www.x.com/', icon: 'x', color: 'white' },
+    { labelKey: 'YouTube', route: 'https://www.youtube.com/', icon: 'youtube', color: '[#FF0000]' },
+  ];
+
   readonly columns: FooterColumn[] = [
     {
       titleKey: 'footer.columns.platform.title',
