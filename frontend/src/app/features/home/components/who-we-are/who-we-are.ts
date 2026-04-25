a7ad71bfa0df6af1df7077d45bae7c405398ccf8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-25 12:38:37 -0300

    feat: adds imports, interfaces and attributes to frontend/src/app/features/home/components/who-we-are/who-we-are.ts

--- frontend/src/app/features/home/components/who-we-are/who-we-are.ts
+++ frontend/src/app/features/home/components/who-we-are/who-we-are.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { NgmMotionDirective } from '@scripttype/ng-motion';
+
+interface Stat {
+  key: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-who-we-are',
-  imports: [],
+  imports: [TranslatePipe, NgmMotionDirective],
   templateUrl: './who-we-are.html',
 })
-export class WhoWeAre {}
+export class WhoWeAre {
+  readonly stats: Stat[] = [
+    { key: 'positiveReviews', value: '24K+' },
+    { key: 'protectedUsers', value: '18M+' },
+    { key: 'affiliatedCompanies', value: '21K+' },
+  ];
+}
