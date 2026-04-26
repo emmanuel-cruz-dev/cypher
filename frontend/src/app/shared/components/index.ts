c7ef99ed4618926a99c0cfd48e475badf3e42323
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-26 12:13:22 -0300

    feat: adds index files to frontend/src/app/shared/

---
+++ frontend/src/app/shared/components/index.ts
@@ -0,0 +1,5 @@
+export * from './cta-banner/cta-banner';
+export * from './footer/footer';
+export * from './navbar/navbar';
+export * from './scroll-to-top/scroll-to-top';
+export * from './sidebar/sidebar';
