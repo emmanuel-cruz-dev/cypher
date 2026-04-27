5d4cf9f90e2b52739952ac5985fa48dd1a4a0a7b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-27 12:24:04 -0300

    feat: modifies mission-item files in frontend/src/app/features/home/components/mission-item/

--- frontend/src/app/features/home/components/mission-item/mission-item.ts
+++ frontend/src/app/features/home/components/mission-item/mission-item.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+
+interface Feature {
+  key: string;
+}
 
 @Component({
   selector: 'app-mission-item',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './mission-item.html',
 })
-export class MissionItem {}
+export class MissionItem {
+  @Input() feature!: Feature;
+}
