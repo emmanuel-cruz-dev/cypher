8a761ebe5cb1849bcc9e6d631e5b54ebeca54812
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-27 10:14:53 -0300

    refactor: modifies shared imports in frontend/src/app/layouts/main-layout/main-layout.ts

--- frontend/src/app/layouts/main-layout/main-layout.ts
+++ frontend/src/app/layouts/main-layout/main-layout.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { Navbar } from '../../shared/components/navbar/navbar';
-import { Footer } from '../../shared/components/footer/footer';
+import { Navbar, Footer } from '../../shared';
 
 @Component({
   selector: 'app-main-layout',
