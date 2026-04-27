c410f60dbda821e760c5d4e097d29d21a9de83b1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-27 12:12:03 -0300

    feat: adds imports and attributes to frontend/src/app/features/home/components/mission/mission.ts

--- frontend/src/app/features/home/components/mission/mission.ts
+++ frontend/src/app/features/home/components/mission/mission.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { NgmMotionDirective } from '@scripttype/ng-motion';
 
 @Component({
   selector: 'app-mission',
-  imports: [],
+  imports: [TranslatePipe, NgmMotionDirective],
   templateUrl: './mission.html',
 })
-export class Mission {}
+export class Mission {
+  features = [
+    { key: 'cyberDefense' },
+    { key: 'networkSecurity' },
+    { key: 'webProtection' },
+    { key: 'accessManagement' },
+  ];
+}
