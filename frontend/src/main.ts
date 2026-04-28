ac144a70d757f8fcf9befbdcffe089eddda76f19
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-28 09:08:41 -0300

    feat: adds swiper register to frontend/src/main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { register } from 'swiper/element/bundle';
+
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+register();
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
