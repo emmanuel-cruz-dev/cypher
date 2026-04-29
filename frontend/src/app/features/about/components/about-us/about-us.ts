89adbceb5483d4fc45a26045304d4caa1ee77f88
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-29 12:42:17 -0300

    feat: updates imports and attributes in frontend/src/app/features/about/components/about-us/about-us.ts

--- frontend/src/app/features/about/components/about-us/about-us.ts
+++ frontend/src/app/features/about/components/about-us/about-us.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
+import { NgmMotionDirective } from '@scripttype/ng-motion';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [TranslatePipe, MatIconModule, NgmMotionDirective],
   templateUrl: './about-us.html',
 })
-export class AboutUs {}
+export class AboutUs {
+  features = [
+    'centralizedCredential',
+    'dataInTransit',
+    'passwordGenerator',
+    'labelsAndCategories',
+    'securityAudit',
+    'secureAuthentication',
+  ];
+}
