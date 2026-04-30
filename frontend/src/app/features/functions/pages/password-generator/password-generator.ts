7001b961286b6a5ec2a1ba54583d8d5393e60c32
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-30 12:55:42 -0300

    feat: adds password-generator files to frontend/src/app/features/functions/pages/password-generator/

---
+++ frontend/src/app/features/functions/pages/password-generator/password-generator.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-generator',
+  imports: [],
+  templateUrl: './password-generator.html',
+})
+export class PasswordGenerator {}
