52dfba8c980c0d20df1c009ca1c23100188ef2bd
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-30 12:54:13 -0300

    feat: adds username-generator files to frontend/src/app/features/functions/pages/username-generator/

---
+++ frontend/src/app/features/functions/pages/username-generator/username-generator.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-username-generator',
+  imports: [],
+  templateUrl: './username-generator.html',
+})
+export class UsernameGenerator {}
