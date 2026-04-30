c6281d160ca5e13d0f1bdd75751c8621503e9738
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-30 12:56:19 -0300

    feat: adds passphrase-generator files to frontend/src/app/features/functions/pages/passphrase-generator/

---
+++ frontend/src/app/features/functions/pages/passphrase-generator/passphrase-generator.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passphrase-generator',
+  imports: [],
+  templateUrl: './passphrase-generator.html',
+})
+export class PassphraseGenerator {}
