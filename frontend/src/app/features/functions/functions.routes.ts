2a337b214314e9ecdb5dfbde206e7228b179d303
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-30 13:11:34 -0300

    feat: adds new routes to frontend/src/app/features/functions/functions.routes.ts

--- frontend/src/app/features/functions/functions.routes.ts
+++ frontend/src/app/features/functions/functions.routes.ts
@@ -1,11 +1,35 @@
 import { Routes } from '@angular/router';
 
 import { Functions } from './pages/functions';
+import { PasswordGenerator } from './pages/password-generator/password-generator';
+import { PasswordStrengthAnalysis } from './pages/password-strength-analysis/password-strength-analysis';
+import { PassphraseGenerator } from './pages/passphrase-generator/passphrase-generator';
+import { UsernameGenerator } from './pages/username-generator/username-generator';
 
 export const FUNCTIONS_ROUTES: Routes = [
   {
     path: '',
     component: Functions,
     title: 'Funciones',
+  },
+  {
+    path: 'password-generator',
+    component: PasswordGenerator,
+    title: 'Generador de contraseñas',
+  },
+  {
+    path: 'password-strength-analysis',
+    component: PasswordStrengthAnalysis,
+    title: 'Análisis de fuerza de contraseña',
+  },
+  {
+    path: 'passphrase-generator',
+    component: PassphraseGenerator,
+    title: 'Generador de frases de contraseña',
+  },
+  {
+    path: 'username-generator',
+    component: UsernameGenerator,
+    title: 'Generador de nombres de usuario',
   },
 ];
