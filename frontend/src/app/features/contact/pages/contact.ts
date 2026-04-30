e072b855b316e3fcb8b7559c9845e9e3d8c84d49
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-30 10:21:57 -0300

    feat: adds ContactInformation to frontend/src/app/features/contact/pages/contact.ts

--- frontend/src/app/features/contact/pages/contact.ts
+++ frontend/src/app/features/contact/pages/contact.ts
@@ -3,10 +3,11 @@ import { TranslatePipe } from '@ngx-translate/core';
 import { NgmMotionDirective } from '@scripttype/ng-motion';
 
 import { ContactForm } from '../components/contact-form/contact-form';
+import { ContactInformation } from '../components/contact-information/contact-information';
 
 @Component({
   selector: 'app-contact',
-  imports: [TranslatePipe, NgmMotionDirective, ContactForm],
+  imports: [TranslatePipe, NgmMotionDirective, ContactForm, ContactInformation],
   templateUrl: './contact.html',
 })
 export class Contact {}
