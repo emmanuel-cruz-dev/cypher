d1e23cf0a3687a202ca5b049a9a43b403de64335
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-30 12:55:16 -0300

    feat: adds password-strength-analysis files to frontend/src/app/features/functions/pages/password-strength-analysis/

---
+++ frontend/src/app/features/functions/pages/password-strength-analysis/password-strength-analysis.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-strength-analysis',
+  imports: [],
+  templateUrl: './password-strength-analysis.html',
+})
+export class PasswordStrengthAnalysis {}
