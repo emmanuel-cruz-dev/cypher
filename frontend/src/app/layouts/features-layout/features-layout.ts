55ed55a2664be61150a56f1aefe2c7a63634bbde
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-30 12:59:17 -0300

    feat: adds features-layout files to frontend/src/app/layouts/features-layout/

---
+++ frontend/src/app/layouts/features-layout/features-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-features-layout',
+  imports: [RouterModule],
+  templateUrl: './features-layout.html',
+})
+export class FeaturesLayout {}
