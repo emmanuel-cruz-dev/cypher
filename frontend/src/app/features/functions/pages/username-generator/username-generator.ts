2d5ca42bd95aa289babb46782b6f541b5cacb92a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-01 11:23:09 -0300

    feat: adds FunctionsHero and heroData to username-generator files in frontend/src/app/features/functions/pages/username-generator/

--- frontend/src/app/features/functions/pages/username-generator/username-generator.ts
+++ frontend/src/app/features/functions/pages/username-generator/username-generator.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import { FunctionsHero, FunctionsHeroData } from '../../components/functions-hero/functions-hero';
+
 @Component({
   selector: 'app-username-generator',
-  imports: [],
+  imports: [FunctionsHero],
   templateUrl: './username-generator.html',
 })
-export class UsernameGenerator {}
+export class UsernameGenerator {
+  heroData: FunctionsHeroData = {
+    i18nPrefix: 'functions.usernameGenerator',
+    imageSrc: 'assets/images/functions/username-generator.avif',
+    imageAlt: 'Username generator tool preview',
+    accentClass: 'text-violet-500',
+  };
+}
