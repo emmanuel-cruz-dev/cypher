fcc49f5f2eb30905161476fe1e943a0e9c946dd4
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-01 11:13:30 -0300

    feat: adds FunctionsHero to passphrase-generator files in frontend/src/app/features/functions/pages/passphrase-generator/

--- frontend/src/app/features/functions/pages/passphrase-generator/passphrase-generator.ts
+++ frontend/src/app/features/functions/pages/passphrase-generator/passphrase-generator.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import { FunctionsHero, FunctionsHeroData } from '../../components/functions-hero/functions-hero';
+
 @Component({
   selector: 'app-passphrase-generator',
-  imports: [],
+  imports: [FunctionsHero],
   templateUrl: './passphrase-generator.html',
 })
-export class PassphraseGenerator {}
+export class PassphraseGenerator {
+  heroData: FunctionsHeroData = {
+    i18nPrefix: 'functions.passphraseGenerator',
+    imageSrc: 'assets/images/functions/passphrase-generator.avif',
+    imageAlt: 'Passphrase generator tool preview',
+    accentClass: 'text-violet-500',
+  };
+}
