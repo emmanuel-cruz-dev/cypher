8e2251bb8a33d35ef0f198fe111b37b1b45b46e4
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-01 10:48:33 -0300

    feat: adds FunctionsHero to password-generator files in frontend/src/app/features/functions/pages/password-generator/

--- frontend/src/app/features/functions/pages/password-generator/password-generator.ts
+++ frontend/src/app/features/functions/pages/password-generator/password-generator.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { FunctionsHero, FunctionsHeroData } from '../../components/functions-hero/functions-hero';
 import { CtaBanner } from '../../../../shared';
 
 @Component({
   selector: 'app-password-generator',
-  imports: [CtaBanner],
+  imports: [FunctionsHero, CtaBanner],
   templateUrl: './password-generator.html',
 })
-export class PasswordGenerator {}
+export class PasswordGenerator {
+  heroData: FunctionsHeroData = {
+    i18nPrefix: 'functions.passwordGenerator',
+    imageSrc: 'assets/images/functions/password-generator.avif',
+    imageAlt: 'Password generator tool preview',
+    accentClass: 'text-violet-500',
+  };
+}
